b89585ae09a878e29d87a7ed1dc2d870bd4ecb02
Emilio Parra <Emilio.pg95@gmail.com>
2022-10-04 19:04:09 -0500

    🚧 Models created

---
+++ api/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+    type: String,
+    required: true,
+},
+    lastName: {
+    type: String,
+},
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+},
+    phone: {
+    type: String,
+},
+    password: {
+    type: String,
+    required: true,
+    },
+});
+
+export default mongoose.model('User', userSchema);
