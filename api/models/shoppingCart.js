a0c8a8d91e8f064fd666a74daada742a1cbbf063
Emilio Parra <Emilio.pg95@gmail.com>
2022-10-04 20:11:14 -0500

    🚧 Cart updated, still missing fields

---
+++ api/models/shoppingCart.js
@@ -0,0 +1,8 @@
+// Product, sub total
+
+import mongoose from "mongoose"
+import shoppingCartSchema = new mongoose.Schema ({
+    
+})
+
+export default mongoose.model('shoppingCart', shoppingCartSchema)
